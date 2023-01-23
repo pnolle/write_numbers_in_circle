1f40307bd876c4d65dfb18e5537f4a72cc039a8f
pnolle <pnolle@web.de>
2023-01-24 00:07:52 +0100

    chatGPT ver 2

--- table.js
+++ table.js
@@ -1,24 +1,18 @@
 let numbers = [];
-
 for (let i = 1; i <= 15; i++) {
     let row = [];
     for (let j = 1; j <= 15 - i; j++) {
         row.push(" ");
     }
-    for (let j = 1; j <= i; j++) {
-        row.push((i - j + 15));
-    }
+    row.push(i + 15);
     numbers.push(row);
 }
-
-for (let i = 1; i <= 15; i++) {
+for (let i = 2; i <= 15; i++) {
     let row = [];
     for (let j = 1; j <= i - 1; j++) {
         row.push(" ");
     }
-    for (let j = 1; j <= 15 - i; j++) {
-        row.push((15 - i + j));
-    }
+    row.push(30 - i);
     numbers.push(row);
 }
 console.table(numbers);
--- write.js
+++ write.js
@@ -3,19 +3,15 @@ for (let i = 1; i <= 15; i++) {
     for (let j = 1; j <= 15 - i; j++) {
         line += " ";
     }
-    for (let j = 1; j <= i; j++) {
-        line += (i - j + 15) + " ";
-    }
+    line += (i + 15) + " ";
     console.log(line);
 }
 
-for (let i = 1; i <= 15; i++) {
+for (let i = 2; i <= 15; i++) {
     let line = "";
     for (let j = 1; j <= i - 1; j++) {
         line += " ";
     }
-    for (let j = 1; j <= 15 - i; j++) {
-        line += (15 - i + j) + " ";
-    }
+    line += (30 - i) + " ";
     console.log(line);
 }
