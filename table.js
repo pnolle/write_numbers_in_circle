794f28264661110e687a66e7f1b8fe9459244cc8
pnolle <pnolle@web.de>
2023-01-24 00:04:29 +0100

    chatGPT write + table 1

--- table.js
+++ table.js
@@ -1,21 +1,24 @@
+let numbers = [];
+
 for (let i = 1; i <= 15; i++) {
-    let line = "";
+    let row = [];
     for (let j = 1; j <= 15 - i; j++) {
-        line += " ";
+        row.push(" ");
     }
     for (let j = 1; j <= i; j++) {
-        line += (i - j + 15) + " ";
+        row.push((i - j + 15));
     }
-    console.log(line);
+    numbers.push(row);
 }
 
 for (let i = 1; i <= 15; i++) {
-    let line = "";
+    let row = [];
     for (let j = 1; j <= i - 1; j++) {
-        line += " ";
+        row.push(" ");
     }
     for (let j = 1; j <= 15 - i; j++) {
-        line += (15 - i + j) + " ";
+        row.push((15 - i + j));
     }
-    console.log(line);
+    numbers.push(row);
 }
+console.table(numbers);
